5dca77741912209f152073435f7c2332e297b83e
Amar Ramdas <amar.ramdas@asrr.nl>
2022-05-23 16:27:25 +0200

    Update IDGenerator and add some exceptions

---
+++ lib/src/main/kotlin/nl/asrr/common/exceptions/InvalidOperationException.kt
@@ -0,0 +1,3 @@
+package nl.asrr.common.exceptions
+
+class InvalidOperationException(override val message: String?) : Exception(message)
---
+++ lib/src/main/kotlin/nl/asrr/common/models/entity/BaseModifierEntity.kt
@@ -0,0 +1,14 @@
+package nl.asrr.common.models.entity
+
+import java.time.ZonedDateTime
+
+/**
+ * Base modifier entity extension for our entities. Stores modification and creation data.
+ * @author Amar Ramdas
+ */
+data class BaseModifierEntity(
+    var modifiedBy: String,
+    var modifiedAt: ZonedDateTime,
+    var createdBy: String,
+    val createdAt: ZonedDateTime = ZonedDateTime.now()
+)
