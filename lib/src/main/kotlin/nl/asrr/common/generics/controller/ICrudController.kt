a993ee38a13b8939405b1f8af52de28f4817bd16
Amar Ramdas <amar.ramdas@asrr.nl>
2022-11-02 15:14:57 +0000

    Update crudcontroller

--- lib/src/main/kotlin/nl/asrr/common/generics/controller/ICrudController.kt
+++ lib/src/main/kotlin/nl/asrr/common/generics/controller/ICrudController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.Operation
 import nl.asrr.common.generics.model.ICrudEntity
 import nl.asrr.common.generics.service.ICrudService
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
+import javax.swing.SortOrder
 
 abstract class ICrudController<T : ICrudEntity>(open val service: ICrudService<T>) {
 
@@ -30,9 +32,14 @@ abstract class ICrudController<T : ICrudEntity>(open val service: ICrudService<T
     }
 
     @GetMapping("/page")
-    @Operation(summary = "Get page by page number and size")
-    open fun getPage(@RequestParam pageNumber: Int = 0, @RequestParam pageSize: Int? = 50): ResponseEntity<Page<T>> {
-        return ResponseEntity.ok(service.find(pageNumber, pageSize))
+    @Operation(summary = "Get page by page number and size with optional sorting based on field to sortBy and direction")
+    open fun getPage(
+        @RequestParam pageNumber: Int = 0,
+        @RequestParam pageSize: Int? = 50,
+        @RequestParam sortBy: String? = null,
+        @RequestParam direction: Sort.Direction = Sort.DEFAULT_DIRECTION
+    ): ResponseEntity<Page<T>> {
+        return ResponseEntity.ok(service.find(pageNumber, pageSize, sortBy, direction))
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
--- lib/src/main/kotlin/nl/asrr/common/generics/service/ICrudService.kt
+++ lib/src/main/kotlin/nl/asrr/common/generics/service/ICrudService.kt
@@ -6,28 +6,64 @@ import nl.asrr.common.generics.model.ICrudEntity
 import nl.asrr.common.generics.repository.ICrudRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 
+/**
+ * Generic service for CRUD operations
+ */
 abstract class ICrudService<T : ICrudEntity>(open val repository: ICrudRepository<T>) {
 
+    /**
+     * Find entity by id
+     * @param id The id of the entity
+     */
     fun find(id: String): T {
         val found = repository.findById(id)
         if (found.isEmpty) throw NotFoundException(id)
         return found.get()
     }
 
-    fun find(pageNumber: Int = 0, pageSize: Int?): Page<T> {
-        return repository.findAll(PageRequest.of(pageNumber, pageSize ?: 50))
+    /**
+     * Find all entities
+     * @param page The page number
+     * @param pageSize The page size
+     * @param sortBy The field to sort by
+     * @param sortDirection The sort direction
+     */
+    fun find(page: Int = 0, pageSize: Int?, sortBy: String? = null, sortDirection: Sort.Direction = Sort.DEFAULT_DIRECTION): Page<T> {
+        val size = pageSize ?: 50
+
+        val pageRequest = sortBy?.let {
+            PageRequest.of(
+                page,
+                size,
+                Sort.by(Sort.Order(sortDirection, sortBy))
+            )
+        } ?: PageRequest.of(page, size)
+
+        return repository.findAll(pageRequest)
     }
 
+    /**
+     * Find all entities
+     */
     fun findAll(): List<T> {
         return repository.findAll()
     }
 
+    /**
+     * Delete entity by id
+     * @param id The id of the entity
+     */
     open fun delete(id: String) {
         repository.deleteById(id)
     }
 
-    fun delete(id: List<String>) {
-        repository.deleteAllById(id)
+    /**
+     * Delete entities by list of ids
+     * @param ids The list of ids
+     */
+    fun delete(ids: List<String>) {
+        repository.deleteAllById(ids)
     }
 }
