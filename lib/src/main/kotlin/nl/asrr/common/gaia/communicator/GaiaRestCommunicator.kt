74304307ce57e155ce6c2a35cff095f6aea61613
Amar Ramdas <amar.ramdas@asrr.nl>
2022-11-22 22:29:16 +0100

    Fix gaia communicator

--- lib/src/main/kotlin/nl/asrr/common/gaia/communicator/GaiaRestCommunicator.kt
+++ lib/src/main/kotlin/nl/asrr/common/gaia/communicator/GaiaRestCommunicator.kt
@@ -2,6 +2,9 @@ package nl.asrr.common.gaia.communicator
 
 import nl.asrr.common.gaia.dto.CreateApplication
 import nl.asrr.common.gaia.dto.NodeUpdate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForEntity
 
@@ -11,14 +14,17 @@ class GaiaRestCommunicator {
         try {
             restTemplate().postForEntity<Any>("https://gaia.kube.asrr.nl/api/v1/application/create", creation)
         } catch (_: Exception) {
+            // ignore
         }
     }
 
-    fun updateNode(update: NodeUpdate) {
-        try {
-            restTemplate().put("https://gaia.kube.asrr.nl/api/v1/application/update-node", update)
-        } catch (_: Exception) {
-        }
+    fun updateNode(update: NodeUpdate) = try {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val entity = HttpEntity<NodeUpdate>(update, headers)
+        restTemplate().put("https://gaia.kube.asrr.nl/api/v1/application/update-node", entity)
+    } catch (_: Exception) {
+        // ignore
     }
 
     private fun restTemplate(): RestTemplate = RestTemplate()
