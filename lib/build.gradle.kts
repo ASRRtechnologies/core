d469f39593608aaf1ccfd9833adec090b259071b
Julius Cathalina <julius.cathalina@gmail.com>
2026-03-09 23:05:34 +0100

    fix: remove incorrect API for maven central publishing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -86,11 +86,6 @@ publishing {
     }
 }
 
-mavenCentralPublishing {
-    publishingType.set("AUTOMATIC")
-    coordinates("nl.asrr", "core", version.toString())
-}
-
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
